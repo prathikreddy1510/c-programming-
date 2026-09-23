d90e18c232ca15455cbd31c6b54fc1de73997478
prathikreddy1510 <prathikreddy1510@gmail.com>
2026-09-23 17:03:46 +0530

    Add files via upload

---
+++ factorial of n.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int i,n,fact=1 ;
+	printf("enter n value\n");
+	scanf("%d",&n);
+	for (i=1;i<=n;i++){
+	
+	fact=fact*i;}
+
+	printf("%d is factorial of n",fact);
+	return 0;
+}
